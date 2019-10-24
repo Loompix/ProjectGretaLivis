65d6216e71f982ce61b802d9ae47e78b19e11062
Loompix <Cyril.livis@gmail.com>
2019-10-24 19:26:26 +0200

    First push

---
+++ public/index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Mon projet Greta</title>
+</head>
+
+
+
+<body>
+    
+    <header>
+        <h1>Projet pour candidature <em>LIVIS Cyril</em></h1>
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+            <button class="navbar-toggler collapsed" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="navbar-collapse collapse" id="navbarSupportedContent" style>
+                <ul class="navbar-nav mx-auto">
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Exemple PHP</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="https://www.linkedin.com/in/cyril-livis" target="_blank">Mon linkedin</a>
+                </li>
+                </ul>
+            </div>
+        </nav>
+    </header>
+
+    <p class="text-center m-3">
+        Bonjour et bienvenue sur cette page spécialement créée pour alimenter mon dossier de candidature pour la formation de développeur web/mobil dispensée par le <a class="link-greta" target="_blank" href="https://www.greta-aquitaine.fr/greta-sud-aquitaine/agences-greta-sud/agence-de-pau/">greta</a>
+    </p>
+</body>
+
+</html>
