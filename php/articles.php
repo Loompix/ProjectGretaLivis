16b74cd31abfb098ea2c6f2cd4decfacb984c8df
Loompix <Cyril.livis@gmail.com>
2019-10-26 17:03:53 +0200

    mise en prod

--- php/articles.php
+++ php/articles.php
@@ -5,20 +5,19 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    <link rel="stylesheet" href="../public/css/style.css">
+    <link rel="stylesheet" href="../css/style.css">
     <title>Exemple PHP</title>
 </head>
 <body>
-    <?php
-    require_once('/var/www/html/Projet greta/php/pdo.php');
+<?php
+    require_once('pdo.php');
     ?>
+    
         <h2> Voici la liste des articles enregistrés en BDD gérés via phpMyAdmin : </h2>
-        <p class="text-center mb-5">Retour à <a class ="text-warning"href="../public/index.php">l'accueil</a></p>
+        <p class="text-center mb-5">Retour à <a class ="text-warning"href="../index.php">l'accueil</a></p>
         
 
         <?php
-            // Si je souhaite afficher les adresses emails de mes auteurs :
-            // Je vais donc parcourir mon tableau de résultats
             foreach ($results as $currentResult) {
 
                 ?>
