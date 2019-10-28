570ecca5627652eaedf83a27a78729a1040bfd81
Loompix <Cyril.livis@gmail.com>
2019-10-28 14:16:02 +0100

    résolutions de bug d'affichage + ajout page cv

--- index.php
+++ index.php
@@ -17,17 +17,20 @@
     <header>
         <h1>Projet pour candidature <em>LIVIS Cyril</em></h1>
         <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-            <button class="navbar-toggler collapsed" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
 
-            <div class="navbar-collapse collapse" id="navbarSupportedContent" style>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent" style>
                 <ul class="navbar-nav mx-auto">
                 <li class="nav-item">
-                    <a class="nav-link" href="../php/articles.php">Exemple PHP</a>
+                    <a class="nav-link" href="php/articles.php">Exemple PHP</a>
                 </li>
                 <li class="nav-item">
                     <a class="nav-link" href="https://www.linkedin.com/in/cyril-livis" target="_blank">Mon linkedin</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="img/CV-LIVIS-Cyril-DEV.pdf" target="_blank">Mon CV</a>
                 </li>
                 </ul>
             </div>
@@ -46,6 +49,11 @@
             Le code produit est disponible en OpenSource sur Github -> <a href="https://github.com/Loompix/ProjectGretaLivis" target="_blank">Ici</a>
         <p>
     </article>
+    
+    <!-- Import des scripts JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 </body>
 
 </html>
